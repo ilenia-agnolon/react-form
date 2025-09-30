8c7432742482f3eae03ebccdcbdcee4ce3284f82
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-30 16:27:37 +0200

    creato file articleslist jsx eimportatoin app jsx

---
+++ src/components/form/ArticlesList.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import titles from "../titles";
+
+function ArticlesList() {
+  const [articles, setArticles] = useState();
+
+  // lista titoli articoli
+  return (
+    <ul>
+      <li>1</li>
+    </ul>
+  );
+}
+
+export default ArticlesList;
