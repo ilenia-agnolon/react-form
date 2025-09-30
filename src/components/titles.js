5760aca977783b9f1ce28f102031560fc8ff7f1d
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-30 15:29:23 +0200

    first commit

---
+++ src/components/titles.js
@@ -0,0 +1,27 @@
+const titles = [
+  "Perché abbiamo le allergie? Potrebbe essere una strategia evolutiva",
+
+  "Il dinosauro ritrovato con il pasto in bocca",
+
+  "Gli animali più strani scoperti nel profondo oceano",
+
+  "Come le piante comunicano sott’acqua",
+
+  "Le molecole che spiegano il “sapore” dell’acqua",
+
+  "Cellule senescenti: l’invecchiamento dentro di noi",
+
+  "Com’è la vita in assenza di gravità? Esperimenti nello spazio",
+
+  "I microrganismi che degradano la plastica: speranza per il futuro",
+
+  "La chimica dietro il cambiamento del colore nell’autunno",
+
+  "Cosa sappiamo dell’energia oscura? Ultime scoperte",
+
+  "Il linguaggio segreto delle api: danza, suoni e feromoni",
+
+  "Virus “gentili”: microrganismi che aiutano il nostro corpo",
+
+  "I fenomeni atmosferici più strani osservati da satellite",
+];
